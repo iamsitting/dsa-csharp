415fb6b7876c72aa928743082c8a6c95598d3ae1
iamsitting <carlos@iamsitting.com>
2023-12-26 11:46:39 -0600

    linear search array

---
+++ PrimeService/LinearSearchArray.cs
@@ -0,0 +1,18 @@
+﻿namespace PrimeService;
+
+public interface ILinearSearchArray
+{
+    public bool Search(int[] array, int val);
+}
+
+public class LinearSearchArray : ILinearSearchArray
+{
+    public bool Search(int[] array, int val)
+    {
+        for(var i = 0; i < array.Length; i++)
+        {
+            if(val == array[i]) return true;
+        }
+        return false;
+    }
+}
